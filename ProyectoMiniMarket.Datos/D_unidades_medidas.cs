a5720a260bf3f9a8fb99f9f61b7c6e87cfc46c01
Victor <victorsalasok@gmail.com>
2023-08-25 01:44:47 -0300

    Se crearon capaz de negocio y entidades para las unidades medidas

---
+++ ProyectoMiniMarket.Datos/D_unidades_medidas.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using ProyectoMiniMarket.Entidades;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProyectoMiniMarket.Datos
+{
+    public class D_unidades_medidas
+    {
+        public DataTable Listado_um(string cTexto)
+        {
+            MySqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            MySqlConnection SQLCon = new MySqlConnection();
+
+            try
+            {
+                SQLCon = Conexion.getInstancia().CrearConexion();
+                MySqlCommand Comando = new MySqlCommand("USP_Listado_um", SQLCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("cTexto", MySqlDbType.VarChar).Value = cTexto;
+                SQLCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SQLCon.State == ConnectionState.Open) SQLCon.Close();
+            }
+        }
+        public string Guardar_um(int nOpcion, E_unidades_medidas oUa)
+        {
+            string Rpta = "";
+            MySqlConnection SqlCon = new MySqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                MySqlCommand Comando = new MySqlCommand("USP_Guardar_um", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("nOpcion", MySqlDbType.Int32).Value = nOpcion;
+                Comando.Parameters.Add("nCodigo_um", MySqlDbType.Int32).Value = oUa.Codigo_um;
+                Comando.Parameters.Add("nAbreviatura_um", MySqlDbType.VarChar).Value = oUa.Abreviatura_um;
+                Comando.Parameters.Add("cDescripcion_um", MySqlDbType.VarChar).Value = oUa.Descripcion_um;
+                
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo registrar los datos";
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
+        }
+        public string Eliminar_um(int Codigo_um)
+        {
+            string Rpta = "";
+            MySqlConnection SqlCon = new MySqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                MySqlCommand Comando = new MySqlCommand("USP_Eliminar_um", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+
+                Comando.Parameters.Add("nCodigo_ma", MySqlDbType.Int32).Value = Codigo_um;
+
+
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo eliminar los datos";
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
+        }
+    }
+}
---
+++ ProyectoMiniMarket.Entidades/E_unidades_medidas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMiniMarket.Entidades
+{
+    public class E_unidades_medidas
+    {
+        public int Codigo_um { get; set; }
+        public string Abreviatura_um { get; set; }
+        public string Descripcion_um { get; set; }
+    }
+}
